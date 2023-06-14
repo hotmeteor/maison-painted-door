16737d882e88ef9ca49aeb0cbba69a3583537369
Adam Campbell <adam@hotmeteor.com>
2023-06-14 13:26:35 -0500

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,vue}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Archivo', ...defaultTheme.fontFamily.sans],
-            }
-        },
+  content: ["./src/**/*.{astro,html,js,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        clay: "#CDD1D4",
+        ocean: "#83AFB8",
+        skyblue: "#DCEAF7",
+      },
+      fontFamily: {
+        sans: ["Archivo", ...defaultTheme.fontFamily.sans],
+        display: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
     },
-    plugins: [
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography')
-    ],
-}
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
