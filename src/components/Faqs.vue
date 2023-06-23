b747a7418eb0dfb8014bb9acdb67943ef3e745bc
Adam Campbell <adam@hotmeteor.com>
2023-06-23 14:01:16 -0500

    Fix GA

--- src/components/Faqs.vue
+++ src/components/Faqs.vue
@@ -10,7 +10,6 @@
       <dt>
         <DisclosureButton
           class="flex w-full items-start justify-between text-left text-gray-900"
-          @click="() => reportConversion(open, faq.gtagId)"
         >
           <span class="text-base font-semibold leading-7">
             {{ faq.question }}
@@ -32,38 +31,26 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/vue/24/outline";
 
-const reportConversion = (open, gtagId) => {
-  if (open) {
-    gtag("event", "conversion", {
-      send_to: gtagId,
-    });
-  }
-};
-
 const faqs = [
   {
     question: "How much does this cost?",
     answer:
       "Standard real estate agent pricing: 3% each from the buyer and seller. This means that you pay nothing if the deal doesn't go through.",
-    gtagId: "AW-11222330237/etLwCLbIs6wYEP3WnOcp",
   },
   {
     question: "Do I have to use your management services?",
     answer:
       "Definitely not. It will make life easier but you're welcome to use whatever management company you want as owners.",
-    gtagId: "AW-11222330237/Q9G9CLnIs6wYEP3WnOcp",
   },
   {
     question: "How can I trust the buyers?",
     answer:
       "The terms of ownership are clearly defined, and our team works to support you throughout your ownership journey.",
-    gtagId: "AW-11222330237/vDewCLzIs6wYEP3WnOcp",
   },
   {
     question: "How can I trust the sellers?",
     answer:
       "Trusting an Airbnb guest or Uber driver has become the norm, but we also understand this is a bit different. With that in mind, we put a lot of effort into validating co-owners are sound using background checks, credit checks, Airbnb host reviews, independent appraiser, etc. <br /><br />Plus, you'll always have the ability to approve or deny any potential co-owner.",
-    gtagId: "AW-11222330237/7waxCL_Is6wYEP3WnOcp",
   },
 ];
 </script>
